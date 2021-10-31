2b6300f8a07d633364c45114c4084be411479ca4
OhGyong <oky7143@naver.com>
2021-11-01 01:56:11 +0900

    스플래시 화면 설정

---
+++ app/src/main/java/com/example/campers/ui/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.campers.ui.splash
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.campers.MainActivity
+import com.example.campers.R
+
+class SplashActivity: Activity() {
+
+    private val duration: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, duration)
+    }
+}
