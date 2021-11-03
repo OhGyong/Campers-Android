a0bacff1e7861c82fc6b9a4e74825f4869f39fe7
OhGyong <oky7143@naver.com>
2021-11-04 01:41:12 +0900

    네이버 지도 추가

--- app/src/main/java/com/example/campers/ui/campingzone/CampingzoneFragment.kt
+++ app/src/main/java/com/example/campers/ui/campingzone/CampingzoneFragment.kt
@@ -6,14 +6,51 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.campers.R
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
-class CampingzoneFragment: Fragment() {
+class CampingzoneFragment: Fragment(), OnMapReadyCallback {
+
+    private lateinit var naverMap: NaverMap // naverMap 선언
+    private lateinit var locationSource: FusedLocationSource // 런타임 권한 처리
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_campingzone, container, false)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+
+    /**
+     * 권한 설정
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
