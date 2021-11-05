daa06507c01453fbafa1123994bd3081576668db
OhGyong <oky7143@naver.com>
2021-11-05 23:53:04 +0900

    다크모드 비활성화 설정

--- app/src/main/java/com/example/campers/MainActivity.kt
+++ app/src/main/java/com/example/campers/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.campers
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.campers.ui.campingzone.CampingzoneFragment
 import com.example.campers.ui.community.CommunityFragment
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // 다크모드 비활성화
         initNavigationBar()
     }
 
