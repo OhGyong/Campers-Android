866b2c4aaec331e5030045aeaf0f8dcfcad0f11a
OhGyong <oky7143@naver.com>
2021-11-08 22:09:02 +0900

    accessToken 유무에 따른 화면 이동처리

--- app/src/main/java/com/example/campers/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/campers/ui/splash/SplashActivity.kt
@@ -5,29 +5,40 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.example.campers.MainActivity
 import com.example.campers.R
 import com.example.campers.ui.login.LoginActivity
+import com.example.campers.util.SharedPreferences
 
-class SplashActivity: Activity() {
 
-    private val duration: Long = 2000
+/**
+ * 스플래시 화면
+ * accessToken을 확인하여 값이 있으면 메인화면으로 이동하고 없으면 로그인화면으로 이동
+ */
+class SplashActivity : Activity() {
+
+    private val duration: Long = 2000 // 2초후에 액티비티 이동
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            val intent = Intent(this, MainActivity::class.java)
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-//            startActivity(intent)
-//            finish()
-//        }, duration)
+        // accessToken이 없으면 로그인화면으로 이동
+        if (SharedPreferences(this@SplashActivity).accessToken == null) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            }, duration)
+        } else { // accessToken 값이 있으면(null이 아니면) 메인화면으로 이동
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            }, duration)
+        }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-            finish()
-        }, duration)
     }
 }
