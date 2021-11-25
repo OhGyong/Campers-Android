be10017068b04526fc0974cf811e3fa49706b613
OhGyong <oky7143@naver.com>
2021-11-25 22:30:07 +0900

    MainActivity에서 changeFragment 대신 navController로 변경

--- app/src/main/java/com/example/campers/MainActivity.kt
+++ app/src/main/java/com/example/campers/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.campers
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.ui.setupWithNavController
 import com.example.campers.ui.campingzone.CampingzoneFragment
 import com.example.campers.ui.community.CommunityFragment
 import com.example.campers.ui.home.HomeFragment
@@ -19,42 +21,51 @@ class MainActivity : AppCompatActivity() {
     private val notificationFragment by lazy { NotificationFragment() }
     private val mypageFragment by lazy { MypageFragment() }
 
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // 다크모드 비활성화
-        initNavigationBar()
+//        initNavigationBar()
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fl_container) as NavHost
+        navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnv_main)
+            .setupWithNavController(navController)
     }
 
-    private fun initNavigationBar() {
-        val bnvMain : BottomNavigationView = findViewById(R.id.bnv_main)
-        bnvMain.run {
-            setOnItemSelectedListener {
-                when (it.itemId) {
-                    R.id.first -> {
-                        changeFragment(homeFragment)
-                    }
-                    R.id.second -> {
-                        changeFragment(campingzoneFragment)
-                    }
-                    R.id.third -> {
-                        changeFragment(communityFragment)
-                    }
-                    R.id.fourth -> {
-                        changeFragment(notificationFragment)
-                    }
-                    R.id.fifth -> {
-                        changeFragment(mypageFragment)
-                    }
-                }
-                true
-            }
-            selectedItemId = R.id.first
-        }
-    }
-
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.fl_container, fragment).commit()
-    }
+//    private fun initNavigationBar() {
+//        val bnvMain : BottomNavigationView = findViewById(R.id.bnv_main)
+//        bnvMain.run {
+//            setOnItemSelectedListener {
+//                when (it.itemId) {
+//                    R.id.homeFragment -> {
+//                        changeFragment(homeFragment)
+//                    }
+//                    R.id.campingzoneFragment -> {
+//                        changeFragment(campingzoneFragment)
+//                    }
+//                    R.id.communityFragment -> {
+//                        changeFragment(communityFragment)
+//                    }
+//                    R.id.notificationFragment -> {
+//                        changeFragment(notificationFragment)
+//                    }
+//                    R.id.mypageFragment -> {
+//                        changeFragment(mypageFragment)
+//                    }
+//                }
+//                true
+//            }
+//            selectedItemId = R.id.homeFragment
+//        }
+//    }
+//
+//    private fun changeFragment(fragment: Fragment) {
+//        supportFragmentManager.beginTransaction()
+//                .replace(R.id.fl_container, fragment).commit()
+//    }
 }
