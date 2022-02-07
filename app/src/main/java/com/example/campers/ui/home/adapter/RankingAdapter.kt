c0c43ffe8196377e997d7b4044b4af3620a46cda
OhGyong <oky7143@naver.com>
2022-02-07 23:52:32 +0900

    랭킹 리스트로 변수명 변경

--- app/src/main/java/com/example/campers/ui/home/adapter/RankingAdapter.kt
+++ app/src/main/java/com/example/campers/ui/home/adapter/RankingAdapter.kt
@@ -3,13 +3,13 @@ package com.example.campers.ui.home.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.campers.data.home.Ranking
+import com.example.campers.data.home.RankingList
 import com.example.campers.databinding.RecyclerHomeRankingBinding
 
-class RankingAdapter(private val list: ArrayList<Ranking>) : RecyclerView.Adapter<RankingAdapter.RankingViewHolder>() {
+class RankingAdapter(private val list: ArrayList<RankingList>) : RecyclerView.Adapter<RankingAdapter.RankingViewHolder>() {
 
     inner class RankingViewHolder(private val binding: RecyclerHomeRankingBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(ranking: Ranking){
+        fun bind(ranking: RankingList){
             binding.rankingItem = ranking
         }
     }
