d37e59c50b59975d8dd10ff64379939fcaa37acf
OhGyong <oky7143@naver.com>
2022-02-21 23:05:12 +0900

    hotCommunityDetail에 Body 추가

--- app/src/main/java/com/example/campers/api/HomeService.kt
+++ app/src/main/java/com/example/campers/api/HomeService.kt
@@ -1,9 +1,11 @@
 package com.example.campers.api
 
+import com.example.campers.data.home.HotCommunityDetailRequest
 import com.example.campers.data.home.HotCommunityDetailResponse
 import com.example.campers.data.home.HotCommunityListResponse
 import com.example.campers.data.home.RankingListResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -16,6 +18,6 @@ interface HomeService {
     fun hotCommunityList(): Call<HotCommunityListResponse>
 
     @POST("/home/hot-contents")
-    fun hotCommunityDetail(): Call<HotCommunityDetailResponse>
+    fun hotCommunityDetail(@Body body: HotCommunityDetailRequest): Call<HotCommunityDetailResponse>
 
 }
