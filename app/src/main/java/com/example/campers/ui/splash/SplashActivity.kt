00bc97b83b81fb0987cb197d7f7f53b54fa30176
OhGyong <oky7143@naver.com>
2022-07-26 22:52:35 +0900

    - 스플래시 화면에 gif 추가

--- app/src/main/java/com/example/campers/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/campers/ui/splash/SplashActivity.kt
@@ -5,9 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.example.campers.MainActivity
 import com.example.campers.R
-import com.example.campers.ui.login.LoginActivity
 import com.example.campers.util.SharedPreferences
 
 
@@ -17,21 +18,26 @@ import com.example.campers.util.SharedPreferences
  */
 class SplashActivity : Activity() {
 
-    private val duration: Long = 2000 // 2초후에 액티비티 이동
+    private val duration: Long = 5000 // 5초후에 액티비티 이동
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val splashLogoGif = findViewById<ImageView>(R.id.splash_logo_gif)
+        Glide.with(this).load(R.raw.splash_gif).into(splashLogoGif)
+
         // accessToken이 없으면 로그인화면으로 이동
         if (SharedPreferences(this@SplashActivity).accessToken == null) {
             Handler(Looper.getMainLooper()).postDelayed({
-                val intent = Intent(this, LoginActivity::class.java)
+                val intent = Intent(this, AppIntroActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
                 startActivity(intent)
                 finish()
             }, duration)
-        } else { // accessToken 값이 있으면(null이 아니면) 메인화면으로 이동
+        }
+        // accessToken 값이 있으면(null이 아니면) 메인화면으로 이동
+        else {
             Handler(Looper.getMainLooper()).postDelayed({
                 val intent = Intent(this, MainActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
