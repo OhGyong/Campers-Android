ff97e4236cd96d67098462529d58924031fac2e5
OhGyong <oky7143@naver.com>
2022-09-04 21:45:08 +0900

    - 모닥불 데이터 바인딩 작업

--- app/src/main/java/com/campers/ui/community/CommunityFragment.kt
+++ app/src/main/java/com/campers/ui/community/CommunityFragment.kt
@@ -5,18 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.campers.R
+import com.campers.databinding.FragmentCommunityBinding
 
 class CommunityFragment: Fragment() {
+
+    // 데이터 바인딩
+    private lateinit var mBinding: FragmentCommunityBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_community, container, false)
+    ): View {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_community, container, false)
+        return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
